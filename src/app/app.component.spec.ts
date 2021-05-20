fa34a1071d3a0622487792c26d36f92185173949
Fabián Sánchez <fadarsaleeing@gmail.com>
2021-05-20 12:03:46 -0500

    feat: first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'equidraulicossas'`, () => {
+  it(`should have as title 'ehidraulicossas'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('equidraulicossas');
+    expect(app.title).toEqual('ehidraulicossas');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('equidraulicossas app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('ehidraulicossas app is running!');
   });
 });
